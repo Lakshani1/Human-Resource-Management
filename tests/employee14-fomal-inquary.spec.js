90908ebc6a7f83629a899bee866a7175a49e7361
lakshani1 <isankahi@gmail.com>
2026-08-20 11:46:10 +0530

    Add employee fomal inquary

---
+++ tests/employee14-fomal-inquary.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://124.43.17.171:4001/');
+  await page.getByRole('button', { name: 'OK' }).click();
+  await page.locator('input[type="text"]').click();
+  await page.locator('input[type="text"]').fill('erp@test');
+  await page.locator('input[type="password"]').click();
+  await page.locator('input[type="password"]').fill('dev@erp@test');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Explore ERP Features' }).click();
+  await page.getByText('Human Resources Management').click();
+});
